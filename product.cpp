4106c89678b3e18f3e884185fbe318b91374daf4
vishals3012 <92639510+vishals3012@users.noreply.github.com>
2022-03-27 11:45:50 +0530

    Product of 2 numbers

--- product.cpp
+++ product.cpp
@@ -1,62 +1,14 @@
-[Project]
-FileName=product.cpp
-Name=Project1
-Type=1
-Ver=2
-ObjFiles=
-Includes=
-Libs=
-PrivateResource=
-ResourceIncludes=
-MakeIncludes=
-Compiler=
-CppCompiler=
-Linker=
-IsCpp=1
-Icon=
-ExeOutput=
-ObjectOutput=
-LogOutput=
-LogOutputEnabled=0
-OverrideOutput=0
-OverrideOutputName=
-HostApplication=
-UseCustomMakefile=0
-CustomMakefile=
-CommandLine=
-Folders=
-IncludeVersionInfo=0
-SupportXPThemes=0
-CompilerSet=3
-CompilerSettings=0000000100000000000000000
-UnitCount=1
+#include <iostream>
+using namespace std;
 
-[VersionInfo]
-Major=1
-Minor=0
-Release=0
-Build=0
-LanguageID=1033
-CharsetID=1252
-CompanyName=
-FileVersion=
-FileDescription=Developed using the Dev-C++ IDE
-InternalName=
-LegalCopyright=
-LegalTrademarks=
-OriginalFilename=
-ProductName=
-ProductVersion=
-AutoIncBuildNr=0
-SyncProduct=1
-
-[Unit1]
-FileName=product.cpp
-CompileCpp=1
-Folder=
-Compile=1
-Link=1
-Priority=1000
-OverrideBuildCmd=0
-BuildCmd=
-
+int main() {
+  double num1, num2, product;
+  cout << "Enter two numbers: ";
+// stores two floating point numbers in num1 and num2 respectively
+  cin >> num1 >> num2;
+ // performs multiplication and stores the result in product variable
+  product = num1 * num2;  
+    cout << "Product = " << product;    
+    
+  return 0;
+}
